f03653208d1e2f73cbd1dce1b2c54f304253883a
banushasarma <sharmabanu03@gmail.com>
2024-08-07 21:03:55 +0530

    Add project files.

---
+++ EMedicineBackEnd/Models/Response.cs
@@ -0,0 +1,18 @@
+﻿namespace EMedicineBackEnd.Models
+{
+    public class Response
+    {
+        public int StatusCode { get; set; }
+        public string StatusMessage { get; set; }
+        public List<Users> listUsers { get; set; }
+        public Users user { get; set; } 
+        public List<Medicine> listMedicines { get; set; }
+        public Medicine medicine { get; set; }
+        public List<OrderItem> listItems { get; set; }
+        public OrderItem orderItem { get; set; }
+        public List<Orders> listOrders { get; set; }
+        public Orders order { get; set; }
+        public List<Cart> listCarts { get; set; }
+        public Cart cart { get; set; }
+    }
+}
